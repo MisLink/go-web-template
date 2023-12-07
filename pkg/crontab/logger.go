a565b31de4a7cfe79ed662e2a1e3a589f700b5e3
MisLink <gjq.uoiai@outlook.com>
2023-12-07 18:40:27 +0800

    Fix crontab logger
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- pkg/crontab/logger.go
+++ pkg/crontab/logger.go
@@ -9,10 +9,10 @@ type Logger struct {
 
 func (l *Logger) Info(msg string, keysAndValues ...any) {
 	if l.logInfo {
-		l.logger.Info().Msgf(msg, keysAndValues...)
+		l.logger.Info().Fields(keysAndValues).Msg(msg)
 	}
 }
 
 func (l *Logger) Error(err error, msg string, keysAndValues ...any) {
-	l.logger.Err(err).Msgf(msg, keysAndValues...)
+	l.logger.Err(err).Fields(keysAndValues).Msg(msg)
 }
